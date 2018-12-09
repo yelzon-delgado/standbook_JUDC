8f31643f0301a2e44b565c5c1baf1c9efb0eaa3d
haywill20 <45340545+haywill20@users.noreply.github.com>
2018-12-08 18:59:49 -0600

    actualizacion

---
+++ validaciones/form_escritor.js
@@ -0,0 +1,8 @@
+function enviar() {
+	var nombre = document.getElementById("nombre").value;
+	var apellido = document.getElementById("apellido").value;
+	var telefono = document.getElementById("telefono").value;
+	var sexo = document.getElementById("sexo").value;
+	
+
+}
