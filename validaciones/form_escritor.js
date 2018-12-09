d3f49a4cb021197ba0cbb0513cbe5aa105d4a24c
haywill20 <45340545+haywill20@users.noreply.github.com>
2018-12-08 19:05:34 -0600

    actualizacion

--- validaciones/form_escritor.js
+++ validaciones/form_escritor.js
@@ -3,6 +3,8 @@ function enviar() {
 	var apellido = document.getElementById("apellido").value;
 	var telefono = document.getElementById("telefono").value;
 	var sexo = document.getElementById("sexo").value;
-	
-
+	var edad = document.getElementById("edad");
+	var direccion = document.getElementById("direccion").value;
+	var contraseña = document.getElementById("contraseña").value;
+	var repetir = document.getElementById("repetir").value;
 }
